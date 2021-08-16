59486322b9bae1095deaa9adce93b6c30a21cb0c
Junho Yeo <hanaro0704@gmail.com>
2021-08-16 20:10:43 +0900

    fix: use `VM.run` instead of `eval`

--- src/nuxt.ts
+++ src/nuxt.ts
@@ -1,6 +1,8 @@
+import { VM } from 'vm2';
+
 export const parseNuxtData = (html: string) => {
   const parsed = html
     .split('<script>window.__NUXT__=')[1]
     .split('</script>')[0]
-  return eval(parsed)
+  return new VM().run(parsed)
 }
